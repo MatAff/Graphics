b1db2fe738163b6c7847ab3637c948beedcceb48
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2024-09-08 17:15:38 -0600

    Latest

--- space/main.py
+++ space/main.py
@@ -2,35 +2,28 @@ import cv2
 import math
 import numpy as np
 import random
+from playsound import playsound
 
 from fps import FPS
 from keys import *
 
-# WIDTH, HEIGHT = 640 * 2, 480 * 2
-WIDTH, HEIGHT = 1366, 768
-# WIDTH, HEIGHT = 640, 480
+WIDTH, HEIGHT = [(1366, 768), (640 * 2, 480 * 2), (640, 480)][2]
 DRAW_HEIGHT = 240
 DRAW_START_HEIGHT = 240
 
 fps = FPS(5)
 
-speed = 1  # 10
-rotation_speed_x = 0.0
-rotation_speed_y = 0.0
-rotation_speed_z = 0.0
-target_x = 0
-target_y = 0
+speed = 5  # 5
+rotation_speed_x, rotation_speed_y, rotation_speed_z = 0.0, 0.0, 0.0
+target_x, target_y = 0, 0
 
 # Consider space -1000 to 1000 box
 # Objects is a matrix: x, y, z (depth), size, r, g, b
-
 # Create initial set of starts
-# TODO: Make width parameter
 objects = np.random.uniform(low=-1000, high=1000, size=(50, 7))
 arr = np.asarray(objects)
 
 def add_stars(arr):
-    # TODO: Make the number of stars to add a parameter
     # TODO: Make dependent on speed
     if random.randint(0, 100) < 20:
         add_arr = np.asarray(np.random.randint(low=-1000, high=1000, size=(1, 7)))
@@ -88,10 +81,10 @@ def draw(frame, arr, h, w, y_start, xi, yi, zi, top=False):
         to_color = lambda v: int(abs(v/1000*255))
         rgb = (to_color(rr), to_color(gg), to_color(bb))
         if xx > -50 and xx < WIDTH + 50 and yy > y_start and yy < y_start + h:
-            cv2.circle(frame, [int(xx), int(yy)], int(abs(ss)), rgb, -1)
+            cv2.circle(frame, [int(xx), int(yy)], int(abs(ss) * 1.25), rgb, -1)
 
 cv2.namedWindow('Frame', cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty('Frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.setWindowProperty('Frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 def handle_key(key):
     global running
@@ -125,40 +118,27 @@ def draw_target(frame, target_x, target_y, key):
     cv2.line(frame, [draw_x, draw_y - 15], [draw_x, draw_y + 15], red, 1)
     if key == SPACE:
         cv2.line(frame, [draw_x, draw_y], [int(WIDTH / 2), HEIGHT], red, 3)
-
-
-def plot_temp_target(frame):
-
-    x_offset = 100
-    y_offset = 100
-
-    # Create a mask with transparency
-    mask = cv2.bitwise_not(cv2.cvtColor(ds, cv2.COLOR_BGR2GRAY))
-    ret, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)  # Example thresholding
-
-    # Extract ROI (region of interest) from the frame
-    roi = frame[y_offset:y_offset+ds.shape[0], x_offset:x_offset+ds.shape[1]]
-
-    # Use bitwise operations for transparency
-    mask = mask.astype(bool)
-    frame[y_offset:y_offset+ds.shape[0], x_offset:x_offset+ds.shape[1]][mask] = (
-        cv2.addWeighted(roi, 0, ds, 1.0, 0)[mask]
-    )
+        playsound('laser-blip.wav', False)
+        return draw_x, draw_y
+    return -100, -100
 
 def draw_obj(frame, obj_arr, h, w): # , , y_start, xi, yi, zi, top=False):
-    """Draw objects across both frame. Don't worry about distortion to begin with."""
+    """Draw objects across both frames. Don't worry about distortion to begin with."""
+    draw_positions = []
     for row in obj_arr:
         xx, yy, zz = row
         top = True if yy > zz else False
         if top:
+            if yy < 0 : continue
             x = xx / zz * h / -2 + w / 2
             y = zz / yy * h / -2 + h / 2
         else:
+            if zz < 0 : continue
             x = xx / zz * h / 2 + w / 2
             y = yy / zz * h / 2 + h / 2 + h
         dist = (xx**2 + yy**2 + zz**2)**0.5
         size = 750 / dist
-        # print(size)
+        draw_positions.append([x, y, size])
 
         ds_show = cv2.resize(ds, (int(ds.shape[1] * size),
                                   int(ds.shape[0] * size)))
@@ -166,17 +146,29 @@ def draw_obj(frame, obj_arr, h, w): # , , y_start, xi, yi, zi, top=False):
         if x < 0 or x > WIDTH or y < 0 or y > HEIGHT:
             break
 
-        # print(x, y)
-
         x_offset = int(x - ds_show.shape[1] / 2)
         y_offset = int(y - ds_show.shape[0] / 2)
+        x_end_offset = x_offset + ds_show.shape[1]
+        y_end_offset = y_offset + ds_show.shape[0]
 
-        # print(x_offset, y_offset)
+        # Trunk image and adjust offset
+        ds_show = ds_show[max(-y_offset, 0):ds_show.shape[0] - y_end_offset + HEIGHT,
+                          max(-x_offset, 0):ds_show.shape[1] - x_end_offset + WIDTH, :]
+        x_offset = x_offset + max(-x_offset, 0)
+        y_offset = y_offset + max(-y_offset, 0)
+
+        # # Create a mask with transparency
+        # # TODO: Calculate mask ahead of time and resize only
+        # mask = cv2.bitwise_not(cv2.cvtColor(ds_show, cv2.COLOR_BGR2GRAY))
+        # ret, mask = cv2.threshold(mask, 5, 255, cv2.THRESH_BINARY)
+        # mask = mask.astype(bool)
 
         # Create a mask with transparency
-        mask = cv2.bitwise_not(cv2.cvtColor(ds_show, cv2.COLOR_BGR2GRAY))
-        ret, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
-        mask = mask.astype(bool)
+        color_min = np.array([147, 100, 0],np.uint8)
+        color_max = np.array([151, 255, 255],np.uint8)
+        HSV  = cv2.cvtColor(ds_show ,cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(HSV, color_min, color_max)
+        mask = ~mask.astype(bool)
 
         # Extract ROI from the frame
         roi = frame[y_offset:y_offset + ds_show.shape[0],
@@ -187,16 +179,25 @@ def draw_obj(frame, obj_arr, h, w): # , , y_start, xi, yi, zi, top=False):
               x_offset:x_offset + ds_show.shape[1]][mask] = (
                   cv2.addWeighted(roi, 0, ds_show, 1.0, 0)[mask]
             )
+    return draw_positions
 
+def check_hit(object_draw_positions, target_x, target_y):
+    for x, y, size in object_draw_positions:
+        print(x, y, target_x, target_y)
+        dist = ((target_x - x)**2 + (target_y - y)**2)**0.5
+        print(dist, size)
+        if dist < size * 50:
+            print('hit')
+            playsound('lo-fi-explosion_16bpm_C_major.wav', False)
 M = get_warp(HEIGHT / 2, WIDTH, WIDTH * 1.25)
 
-# ds = cv2.imread("deathstar.jpeg")
-# ds = cv2.imread("ds2.jpeg")
-ds = cv2.imread("cow2.jpeg")
+ds = cv2.imread("ds2.jpeg")
+# ds = cv2.imread("cow3.jpeg")
+# ds = cv2.imread("space_cow.jpeg")
+# ds = cv2.cvtColor(ds_in, cv2.COLOR_RGB2BGR)
 assert ds is not None
-# image = cv2.resize(image, (frame.shape[1], frame.shape[0]))
 
-obj_arr = np.asarray([[0, -500, 1000]])
+obj_arr = np.asarray([[500, 0, 1000]])
 
 key = -1
 running = True
@@ -219,22 +220,15 @@ while running:
     top_frame = np.zeros((int(HEIGHT / 2), WIDTH, 3), dtype = np.uint8)
     draw(top_frame, arr, HEIGHT / 2, WIDTH, 0, 0, 2, 1, True)
     # cv2.line(top_frame, [250, 50], [250, 250], (255, 0, 0), 4)
-    # cv2.line(top_frame, [WIDTH - 50, 50], [WIDTH - 50, 250], (255, 0, 0), 4)
     top_frame = cv2.warpPerspective(top_frame, M, (top_frame.shape[1], top_frame.shape[0]))
     frame = cv2.vconcat([top_frame, front_frame])
 
-    # plot_temp_target(frame)
-
-    draw_obj(frame, obj_arr, HEIGHT / 2, WIDTH)
-
-    draw_target(frame, target_x, target_y, key)
-
+    object_draw_positions = draw_obj(frame, obj_arr, HEIGHT / 2, WIDTH)
+    draw_x, draw_y = draw_target(frame, target_x, target_y, key)
+    check_hit(object_draw_positions, draw_x, draw_y)
     add_window(frame)
-
     key = show(frame)
-
     handle_key(key)
-
     fps.update(True)
 
 cv2.destroyAllWindows()
--- space/readme.md
+++ space/readme.md
@@ -3,8 +3,25 @@
 # 2024-04-21: 1.0
 
 # --- TODO ---
+
+# Try updated position of projector
+# Add fire on impact
+# Include random turns when no input
+# Manage the object lifecycle
+# Improve images
+
+# --- TOMAYBELATER ---
+
+# Re-evaluate the perception
+# - This will be really hard to get right, (sepearte research project if there is time)
+
+
+# --- TODONE ---
+
+# Add sound on impact >> DONE
+# Prevent crash when object reaches edge >> DONE
 # Experiment with trapezoid transformation >> DONE
 # Initial commit >> DONE
 # Add space frame >> DONE
 # Fix rotation bug >> DONE
-# Move draw to bottom part and parameterize
+# Move draw to bottom part and parameterize >> DONE
